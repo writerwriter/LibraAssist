bd83c9e755a2a9cfa9212d2236e7aa0016cc2dc8
Wr1t3R <catchingfire80@gmail.com>
2017-10-10 22:34:14 +0800

    Initial commit

---
+++ app/src/main/java/com/writerwriter/libraassist/MainActivity.java
@@ -0,0 +1,77 @@
+package com.writerwriter.libraassist;
+
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    private ViewPager viewPager;
+
+    MainFragment mainFragment;
+    CollectionFragment collectionFragment;
+    BorrowFragment borrowFragment;
+    MenuItem preMenuItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        viewPager.setCurrentItem(item.getOrder());
+                        return true;
+                    }
+                });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener(){
+            @Override
+            public void onPageScrolled(int position, float positionOffset,int positionOffsetPixels){
+
+            }
+            @Override
+            public void onPageSelected(int position){
+                if(preMenuItem != null){
+                    preMenuItem.setChecked(false);
+                }
+                else{
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                Log.d("page", "OnPageSelected: "+position);
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                preMenuItem = bottomNavigationView.getMenu().getItem(position);
+            }
+            @Override
+            public void onPageScrollStateChanged(int state){
+
+            }
+        });
+        setupViewPager(viewPager);
+    }
+    private void setupViewPager(ViewPager viewPager){
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        mainFragment = new MainFragment();
+        collectionFragment = new CollectionFragment();
+        borrowFragment = new BorrowFragment();
+        adapter.addFragment(mainFragment);
+        adapter.addFragment(collectionFragment);
+        adapter.addFragment(borrowFragment);
+        viewPager.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/writerwriter/libraassist/ViewPagerAdapter.java
@@ -0,0 +1,31 @@
+package com.writerwriter.libraassist;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Larry on 2017/10/10.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+    @Override
+    public Fragment getItem(int position){
+        return mFragmentList.get(position);
+    }
+    @Override
+    public int getCount(){
+        return mFragmentList.size();
+    }
+    public void addFragment(Fragment fragment){
+        mFragmentList.add(fragment);
+    }
+}
